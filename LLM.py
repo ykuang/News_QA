c68c97879750cbd9fefdb6d068dade5662d0f933
ykuang <517810962@qq.com>
2024-01-16 16:32:14 +0800

    Add python files

---
+++ LLM.py
@@ -0,0 +1,37 @@
+from langchain.llms.base import LLM
+from typing import Any, List, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+class InternLM_LLM(LLM):
+    # 基于本地 InternLM 自定义 LLM 类
+    tokenizer : AutoTokenizer = None
+    model: AutoModelForCausalLM = None
+
+    def __init__(self, model_path :str):
+        # model_path: InternLM 模型路径
+        # 从本地初始化模型
+        super().__init__()
+        print("正在加载模型...")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+        self.model = self.model.eval()
+        print("完成模型的加载")
+
+    def _call(self, prompt : str, stop: Optional[List[str]] = None,
+                run_manager: Optional[CallbackManagerForLLMRun] = None,
+                **kwargs: Any):
+        # 重写调用函数
+        system_prompt = """You are an AI assistant whose name is News_QA (新闻问答小助手).
+        - News_QA (新闻问答小助手) 是由 ykuang 开发的会话语言模型, 旨在帮助人们查找相关新闻信息。
+        - News_QA (新闻问答小助手) 可以理解用户选择的语言，如中文和英文，并能流畅地与用户交流。
+        """
+        
+        messages = [(system_prompt, '')]
+        response, history = self.model.chat(self.tokenizer, prompt , history=messages)
+        return response
+        
+    @property
+    def _llm_type(self) -> str:
+        return "InternLM"
